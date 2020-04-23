1fd4c35268d41a6915b6d27f90b55fee90a93e53
375990012@qq.com <Zhenggjay>
2020-04-23 10:27:39 +0800

    更新使用接口传参

---
+++ Entry_PartTime.py
@@ -0,0 +1,66 @@
+#转正申请申请
+import threading,requests,json,time,sys,re
+from datetime import datetime,timedelta
+from dateutil.relativedelta import relativedelta
+from urllib import parse
+headers = {'Content-Type': 'application/json;charset=UTF-8'}
+
+def token(username,url='test'):
+    # 拿到token
+    url = 'http://%s-ms.xgjk.info/cms-auth/auth/app/login'%url#"http://pre-ms.xgjk.info/cms-auth/auth/app/login"#
+    body = {"username":username , "password": "888888", "isBind": "NO"}  # 创建人
+    try:
+        token_data = requests.post(url=url, headers=headers, data=json.dumps(body)).json()
+        if int(token_data.get('code')) == 0000:
+            # 操作人和审核人后续写成变量动态生成，讲解先暂时写死
+            if body.get('username') == "ua0000002158":
+                print('操作人:艾丽斯  登录时间:%s  登录状态:登录成功' % time.strftime('%Y-%m-%d %H:%M:%S'))
+                token = token_data.get('value').get('token')
+                return token
+            if body.get('username') == "ua0000003764":
+                print('操作人:蒋庆富  登录时间:%s  登录状态:登录成功' % time.strftime('%Y-%m-%d %H:%M:%S'))
+                token = token_data.get('value').get('token')
+                return token
+            elif body.get('username') == "ua0000002156":
+                print('审核人:王丽娜  登录时间:%s  登录状态:登录成功' % time.strftime('%Y-%m-%d %H:%M:%S'))
+                token = token_data.get('value').get('token')
+                return token
+            else:
+                print("其他用户登录时间:%s  登录状态:登录成功：" % time.strftime('%Y-%m-%d %H:%M:%S'))
+                token = token_data.get('value').get('token')
+                return token
+        else:
+            print('%s  登录失败' % time.strftime('%Y-%m-%d %H:%M:%S'))
+            sys.exit()
+    except json.decoder.JSONDecodeError:
+        print("正在构建环境....")
+        sys.exit()
+#获取审核通过的新员工入职去做转正操作
+def get_employee():
+    n=0
+    headers_one = {'Content-Type': 'application/json;charset=UTF-8','Authorization':token("ua0000003764",'pre')}
+    for i in range(0,6):
+        #InApplicationurl='http://pre-ms.xgjk.info/cms-hr-service/org/employeeEntryApply/findByQuery?pageSize=10&pageNumber=%s&companyId=1&billState=AUDIT_PASS&showWaitAudit=NO'%i#替换页数
+        InApplicationurl='http://pre-ms.xgjk.info/cms-hr-service/org/employeeEntryApply/findByQuery?pageSize=10&pageNumber=%s&companyId=1&billState=AUDIT_PASS&showWaitAudit=NO'%i#修改公司id
+        info=requests.get(InApplicationurl,headers_one).json()['result']
+        for i in info:
+            n+=1
+            value=i['id']#单据id
+            employeeName=i['employeeName']#获取名称
+            postId=i['postId']#职位id
+            departmentId=i['departmentId']#获取部门id
+            get_time='http://pre-ms.xgjk.info/cms-hr-service/org/employeeEntryApply/getFullTimeApply?id=%s'%value
+            times = requests.get(get_time, headers_one).json()['employeeCompanyApply']['entryTime']
+            name=parse.quote(employeeName)
+            get_employeeidurl = 'http://pre-ms.xgjk.info/cms-basic-service/account/find?employeeName={}&pageNumber=1&pageSize=10'.format(name)
+            employeeid=requests.get(get_employeeidurl, headers_one).json()['result'][0]['employeeId']
+            #利用datetime.strptime(times, "%Y-%m-%d %H:%M:%S")1.把str格式转换成时间格式str 转datetime.datetime，2.再去切掉后面时分秒,.strftime("%Y-%m-%d")转成字符串形式
+            trial_time=(datetime.strptime(times, "%Y-%m-%d %H:%M:%S")+ timedelta(days=90)).strftime("%Y-%m-%d")
+            print(employeeName,trial_time,'employeeid:',employeeid)
+            positiveurl='http://pre-ms.xgjk.info/cms-hr-service/org/employee/official/save'
+            body={"employeeId":employeeid,"fileUrl":"","officialExplain":"","officialTime":trial_time,"submitWorkflow":False}
+            get_positive=requests.post(positiveurl, headers=headers_one,data=json.dumps(body)).json()
+            print(get_positive)
+if __name__=='__main__':
+    print('go')
+    get_employee()
--- test_allure_demo.py
+++ test_allure_demo.py
@@ -2,7 +2,9 @@
 #@Time:2020/4/14 19:31
 #@Auther:GJ
 import allure
+import requests,json
 import pytest
+from Entry_PartTime import token
 
 @allure.step("步骤1:点xxx")
 def step_1():
@@ -16,6 +18,18 @@ def step_2():
 class TestEditPage():
     '''编辑页面'''
 
+    def setup(self):
+        self.result = None
+        self.verify_info = None
+        self.verify_result = None
+        self.request_param = None
+
+    def teardown(self):
+        desc = "<font color='red'>请求参数:</br> </font>{}</br>" \
+               "<font color='red'>实际结果:</br> </font>{}</br>" \
+               "<font color='red'>验证结果:</br> </font>{}</br>" \
+               "<font color='red'>验证信息:</br> </font>{}".format(self.request_param, self.result, self.verify_result,
+                                                               self.verify_info)
     @allure.story("这是一个xxx的用例")
     def test_1(self, login):
         '''用例描述：先登录，再去执行xxx'''
@@ -25,9 +39,23 @@ class TestEditPage():
 
 
     @allure.story("打开a页面")
-    def test_2(self, login):
+    def test_2(self):
         '''用例描述：先登录，再去执行yyy'''
-        print("yyy")
+        headers_one = {'Content-Type': 'application/json;charset=UTF-8', 'Authorization': token("ua0000003764")}
+        employeeId = 1851  #
+        body = {"employeeId": employeeId, "leaveCompanyIds": ["1", "2"],
+                "leaveOfficeType": "INITIATIVE_LEAVE_JOB", "fileList":
+                    [{"fileName": "图片1",
+                      "fileUrl": "https://default.test.file.dachentech.com.cn/image/app/202001071555037330.jpg"}],
+                "leaveTime": "2020-01-07 14:08:00", "hasKnow": True, "hasNeedLeaveProve": False,
+                "leaveReason": "1234567", "submitWorkflow": True}
+        url = 'https://test-ms.xgjk.info/cms-hr-service/org/areaLeave/save'
+        get_post = requests.post(url=url, data=json.dumps(body), headers=headers_one).json()
+        self.request_param = body
+        self.result = get_post
+        self.verify_info = get_post['code']
+        self.verify_result = get_post['message']
+        assert get_post['code'] == '9999'
 @allure.feature("新增页面")
 class TestAddPage():
     '''编辑页面'''
@@ -39,15 +67,37 @@ class TestAddPage():
         step_2()
         print("xxx")
 
-
+    @allure.dynamic.story
     @allure.story("新增内容提交")
     def test_2(self, login):
         '''用例描述：新增提交'''
         print("新增内容提交")
 
     @allure.epic("史诗")
+    @allure.description_html("""
+    <h1>Test something</h1>
+    <table style='width:100%'>
+    <tr>
+    <th>Firstname</th>
+    <th>lastname</th>
+    <th>Agename</th>
+    </tr>
+    <tr align="center">
+    <td>Wiilon</td>
+    <td>cost</td>
+    <td>28</td>
+    </tr>
+    <tr align="center">
+    <td>pytest</td>
+    <td>niuGG</td>
+    <td>60</td>
+    </tr>
+    """)
     def test_3(self, login):
         '''用例描述：新增提交'''
         print("史诗")
 
-   
+
+
+
+
